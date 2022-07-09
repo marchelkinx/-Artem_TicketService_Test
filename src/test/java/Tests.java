0b16d5ac07c842d9a83308cf3b17748f3bd954a9
marchelkinx <marchelkinx@gmail.com>
2022-07-09 14:01:13 +0300

    PreCondition

---
+++ src/test/java/Tests.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition(){
+        wd = new ChromeDriver();
+//        wd.get("https://ticket-service-69443.firebaseapp.com/");
+        wd.navigate().to("https://ticket-service-69443.firebaseapp.com/");
+
+    }
+
+    @Test
+    public void TestOne(){
+
+    }
+
+    @AfterMethod
+    public void postCondition(){
+
+    }
+
+}
